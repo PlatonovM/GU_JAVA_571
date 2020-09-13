69680c25871d665217fc17777d7869d0f2d60d1b
Mikhail Platonov <miha553@bk.ru>
2020-09-13 20:01:06 +0300

    Lessons2_5

---
+++ src/Lesson2_5/Main.java
@@ -0,0 +1,14 @@
+package Lesson2_5;
+/**
+ * Первое время 23074
+ * Второе время 6439
+ */
+public class Main {
+//    static final int size = 10000000;
+//    static final int h = size / 2;
+
+    public static void main(String[] args) {
+        new ArrayCalc().doOne();
+        new ArrayCalc().doTwo();
+    }
+}
